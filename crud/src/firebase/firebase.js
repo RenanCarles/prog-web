a5b5c2324d6f7ab33e4500bb1e241f2fc0c9caab
RenanCarles <renan.carles@gmail.com>
2025-07-07 15:57:36 -0300

    FirebaseConfig

--- crud/src/firebase/firebase.js
+++ crud/src/firebase/firebase.js
@@ -1,13 +1,12 @@
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyChzosh-nGrUkvLls6OV2-t-IvvDz_ixmM",
-    authDomain: "trab-final-progweb.firebaseapp.com",
-    projectId: "trab-final-progweb",
-    storageBucket: "trab-final-progweb.firebasestorage.app",
-    messagingSenderId: "685164976692",
-    appId: "1:685164976692:web:ecab72ff3672f5b4134861",
-    measurementId: "G-W9NS11BX9G"
-}
+  apiKey: "AIzaSyCI-DR-aUJaLAJeGNgcAqpQ_jZ3Vhred-w",
+  authDomain: "crud-55412.firebaseapp.com",
+  projectId: "crud-55412",
+  storageBucket: "crud-55412.firebasestorage.app",
+  messagingSenderId: "117087425689",
+  appId: "1:117087425689:web:05eb16725c27c904141408"
+};
 
 export const firebaseApp = initializeApp(firebaseConfig)
